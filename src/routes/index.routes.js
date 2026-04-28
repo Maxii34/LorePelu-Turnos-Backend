fe316eb2f0bfbbfaedafceed97badd0262f549fc
Maxi Ordoñez <exemaxi@outlook.com>
2026-04-28 15:42:19 -0300

    Agregar configuracion para conectar a la base de datos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
+//Agregar las rutas aquí
 router.get("/", (req, res) => {
   res.json({ message: "Bienvenido a la API de LorePelu" });
 });
