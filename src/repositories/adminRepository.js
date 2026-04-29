435ae4e1570d6968a152e533f286a6f862323eaa
Maxi Ordoñez <exemaxi@outlook.com>
2026-04-29 08:50:24 -0300

    feat: Agregado de adminRepository e adminService

---
+++ src/repositories/adminRepository.js
@@ -0,0 +1,27 @@
+import Admin from "../model/adminModelo";
+
+const crear = async (adminData) => {
+  return await Admin.create(adminData);
+};
+
+const obtenerTodos = async () => {
+  return await Admin.find().select("-password");
+};
+
+const obtenerPorId = async (id) => {
+  return await Admin.findById(id).select("-password");
+};
+
+const obtenerPorEmail = async (email) => {
+  return await Admin.findOne({ email });
+};
+
+const actualizar = async (id, adminData) => {
+  return await Admin.findByIdAndUpdate(id, adminData, { new: true }).select("-password",);
+};
+
+const eliminar = async (id) => {
+  return await Admin.findByIdAndDelete(id)
+};
+
+export default { crear, obtenerTodos, obtenerPorId, obtenerPorEmail, actualizar, eliminar };
