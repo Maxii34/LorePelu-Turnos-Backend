4fb7795dfe4cb9952dc3b22426b4579bdb0e85a3
Maxi Ordoñez <exemaxi@outlook.com>
2026-05-22 14:03:42 -0300

    feat: Agregar validar token e validar roles en rutas de servicios

--- src/routes/servicio.routes.js
+++ src/routes/servicio.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express";
 import servicioController from "../controllers/servicioController.js";
+import { validarToken } from "../middlewares/validarToken.js";
+import { permitirRoles } from "../middlewares/validarRoles.js";
 
 const router = Router();
 
 //http://localhost:3000/api/servicio
 router
   .route("/")
-  .post(servicioController.crearServicio)
-  .get(servicioController.obtenerServicios);
+  .post(validarToken, permitirRoles(['administrador', 'moderador']), servicioController.crearServicio)
+  .get(validarToken, permitirRoles(['administrador', 'moderador']), servicioController.obtenerServicios);
+  
 //http://localhost:3000/api/servicio/id
 router
   .route("/:id")
-  .get(servicioController.obtenerServicio)
-  .put(servicioController.actualizarServicio)
-  .delete(servicioController.eliminarServicio);
+  .get(validarToken, permitirRoles(['administrador', 'moderador']), servicioController.obtenerServicio)
+  .put(validarToken, permitirRoles(['administrador', 'moderador']), servicioController.actualizarServicio)
+  .delete(validarToken, permitirRoles(['administrador', 'moderador']), servicioController.eliminarServicio);
 
 export default router;
